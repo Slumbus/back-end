b1c736267d577fa0ec53f7fe69b01f36c09c2545
mk020 <mk02081@naver.com>
2024-05-01 13:42:15 +0900

    Initial Setting

---
+++ src/main/java/com/firefly/slumbus/SlumbusApplication.java
@@ -0,0 +1,13 @@
+package com.firefly.slumbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlumbusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlumbusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firefly/slumbus/SlumbusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firefly.slumbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlumbusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
