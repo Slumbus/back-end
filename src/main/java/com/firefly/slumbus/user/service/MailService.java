f75587d87c6f5510014f263f45d69ba990750dde
ygreee <ygreee0320@naver.com>
2024-06-17 17:49:26 +0900

    feat: 이메일 인증번호 전송 시도

---
+++ src/main/java/com/firefly/slumbus/user/service/MailService.java
@@ -0,0 +1,41 @@
+package com.firefly.slumbus.user.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.mail.javamail.JavaMailSender;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MailService {
+
+    private final JavaMailSender emailSender;
+
+    public void sendEmail(String toEmail, String title, String text) {
+        SimpleMailMessage emailForm = createEmailForm(toEmail, title, text);
+        try {
+            emailSender.send(emailForm);
+            log.debug("전송됨");
+        } catch (RuntimeException e) {
+            log.debug("MailService.sendEmail exception occur toEmail: {}, " +
+                    "title: {}, text: {}", toEmail, title, text);
+            //throw new BusinessLogicException(ExceptionCode.UNABLE_TO_SEND_EMAIL);
+            throw new IllegalArgumentException("메일 전송 오류" + e.getMessage(), e);
+        }
+    }
+
+    // 발신할 이메일 데이터 세팅
+    private SimpleMailMessage createEmailForm(String toEmail, String title, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setSubject(title);
+        message.setText(text);
+
+        return message;
+    }
+
+}
--- src/main/java/com/firefly/slumbus/user/service/RegisterService.java
+++ src/main/java/com/firefly/slumbus/user/service/RegisterService.java
@@ -3,21 +3,35 @@ package com.firefly.slumbus.user.service;
 import com.firefly.slumbus.user.dto.request.UserRequestDTO;
 import com.firefly.slumbus.user.entity.UserEntity;
 import com.firefly.slumbus.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.Random;
 
 @Service
 public class RegisterService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private static final String AUTH_CODE_PREFIX = "AuthCode ";
+    private final MailService mailService;
 
-    public RegisterService(UserRepository userRepository) {
+    private final RedisService redisService;
+
+    @Value("${spring.mail.auth-code-expiration-millis}")
+    private long authCodeExpirationMillis;
+
+    public RegisterService(UserRepository userRepository, MailService mailService, RedisService redisService) {
         this.userRepository = userRepository;
+        this.mailService = mailService;
+        this.redisService = redisService;
         this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
@@ -50,4 +64,48 @@ public class RegisterService {
             throw new IllegalArgumentException("이미 가입된 유저입니다.");
         }
     }
+
+    /**
+     * 이메일 인증
+     */
+    public void sendCodeToEmail(String toEmail) {
+        this.checkDuplicatedEmail(toEmail);
+        String title = "Travel with me 이메일 인증 번호";
+        String authCode = this.createCode();
+        mailService.sendEmail(toEmail, title, authCode);
+        // 이메일 인증 요청 시 인증 번호 Redis에 저장 ( key = "AuthCode " + Email / value = AuthCode )
+        redisService.setValues(AUTH_CODE_PREFIX + toEmail,
+                authCode, Duration.ofMillis(this.authCodeExpirationMillis));
+    }
+
+    private void checkDuplicatedEmail(String email) {
+        Optional<UserEntity> existingUser = userRepository.findByEmail(email);
+        if (existingUser.isPresent()) {
+            throw new IllegalArgumentException("이미 가입된 유저입니다.");
+        }
+    }
+
+    private String createCode() {
+        int lenth = 6;
+        try {
+            Random random = SecureRandom.getInstanceStrong();
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < lenth; i++) {
+                builder.append(random.nextInt(10));
+            }
+            return builder.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalArgumentException("코드 생성 오류");
+        }
+    }
+
+    public boolean verifiedCode(String email, String authCode) {
+        this.checkDuplicatedEmail(email);
+        String redisAuthCode = redisService.getValues(AUTH_CODE_PREFIX + email);
+        boolean authResult = redisService.checkExistsValue(redisAuthCode) && redisAuthCode.equals(authCode);
+
+        //return EmailVerificationResult.of(authResult);
+        return authResult;
+    }
+
 }
