f3a278a409bd37f6fbf8c4c07eae3e15c9117b9a
ygreee <ygreee0320@naver.com>
2024-07-05 22:46:42 +0900

    feat: 마이페이지 비밀번호 변경 api

--- src/main/java/com/firefly/slumbus/user/service/MyPageService.java
+++ src/main/java/com/firefly/slumbus/user/service/MyPageService.java
@@ -4,4 +4,6 @@ import com.firefly.slumbus.user.dto.response.MyPageResponseDTO;
 
 public interface MyPageService {
     MyPageResponseDTO getUserById(Long userId);
+
+    boolean patchPassword(Long userId, String originPassword,String newPassword);
 }
