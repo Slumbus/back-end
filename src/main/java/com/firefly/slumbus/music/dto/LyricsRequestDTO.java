a8f767c73481635bb7928fee2c29ad95b683f7c1
mk020 <mk02081@naver.com>
2024-07-14 21:39:28 +0900

    feat: ChatGPT 가사 생성 기능

---
+++ src/main/java/com/firefly/slumbus/music/dto/LyricsRequestDTO.java
@@ -0,0 +1,15 @@
+package com.firefly.slumbus.music.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LyricsRequestDTO {
+
+    private String kidName;
+}
